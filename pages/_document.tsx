8b4f52a5a2a76f5d437dac264454e9c2b738eb21
mrLuisFer <mrluisfeer@gmail.com>
2022-01-15 11:41:57 -0600

    create custom document for styled components

---
+++ pages/_document.tsx
@@ -0,0 +1,33 @@
+import { AppType, RenderPage } from "next/dist/shared/lib/utils";
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export default class MyDocument extends Document {
+  static async getStaticProps(ctx: DocumentContext) {
+    const sheet: ServerStyleSheet = new ServerStyleSheet();
+    const originalRenderPage: RenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: AppType) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps: DocumentInitialProps = await Document.getInitialProps(
+        ctx
+      );
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
