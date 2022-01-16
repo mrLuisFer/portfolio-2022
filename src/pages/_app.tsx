fc8724fdfbf9b39eb605b5538afea3743a4d0181
mrLuisFer <mrluisfeer@gmail.com>
2022-01-15 21:52:05 -0600

    add basic styles and structure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
+import SideBar from "src/components/common/SideBar"
+import { MainContainer } from "src/styles/pages/app.styles"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <MainContainer>
+      <SideBar />
+      <Component {...pageProps} />
+    </MainContainer>
+  )
 }
---
+++ src/styles/pages/app.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const MainContainer = styled.main`
+	display: grid;
+	max-width: 1440px;
+	position: relative;
+	grid-template-columns: 200px 1fr;
+	gap: 0 15px;
+`
